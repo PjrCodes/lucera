0964b61c0f475292f3663a35111f38006a02823e
PjrCodes <25995849+PjrCodes@users.noreply.github.com>
2025-05-29 15:13:36 +0530

    clean up default repo - now empty

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => (
+  <footer className="bg-gray-800 text-white text-center py-4">
+    <p className="text-sm">
+      &copy; Pranjal Rastogi&apos;s LMS Software 2025. All rights reserved.
+    </p>
+  </footer>
+);
+
+export default Footer;
