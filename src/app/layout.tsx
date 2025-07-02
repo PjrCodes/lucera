13619fe2e5d458626858e732639983bc8cfcaf93
PjrCodes <25995849+PjrCodes@users.noreply.github.com>
2025-07-02 10:45:50 +0530

    fonts work now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,20 @@
 import type { Metadata } from "next";
-import { Raleway, Urbanist } from "next/font/google";
+import { Lato, Urbanist } from "next/font/google";
 import "@/app/globals.css";
 import React from "react";
 import ClientProviders from "@/components/core/client-providers";
 import { SessionProvider } from "next-auth/react";
 
 const urbanist = Urbanist({
-  variable: "--font-header",
+  variable: "--font-urbanist",
   subsets: ["latin"],
+  weight: "variable",
 });
 
-const raleway = Raleway({
-  variable: "--font-body",
+const lato = Lato({
+  variable: "--font-lato",
   subsets: ["latin"],
+  weight: ["100", "300", "400", "700","900"],
 });
 
 export const metadata: Metadata = {
@@ -27,11 +29,11 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${urbanist.variable} ${raleway.variable} antialiased`}>
+    <html lang="en" className={`${urbanist.variable} ${lato.variable} antialiased`}>
+      <body className="font-body">
         <ClientProviders>
           <SessionProvider>
-            <div className="font-body w-full min-h-screen flex flex-col bg-primary-50">
+            <div className="w-full min-h-screen flex flex-col bg-primary-50">
               {children}
             </div>
           </SessionProvider>
